c61f934b1e4108442002ead250e48d3c4b80b385
Krishna Kishore Shetty <kkshetty.work@pm.me>
2022-11-05 14:12:11 -0400

    Added Docusaurus files
    Signed-off-by: Krishna Kishore Shetty <kkshetty.work@pm.me>

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/home/kali/Documents/workshop/test-blog/node_modules/infima/dist/css/default/default.css'),
+  require('/home/kali/Documents/workshop/test-blog/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/home/kali/Documents/workshop/test-blog/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/home/kali/Documents/workshop/test-blog/src/css/custom.css'),
+];
